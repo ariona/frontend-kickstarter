d739b76a89972cb5151eab8991994143ebab6f71
arionarian <yans_fied@yahoo.com>
2016-09-06 09:41:02 +0700

    Fix browser refresh when editing sass after compiling jade

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp          = require( 'gulp' ),
 	sass          = require( 'gulp-sass' ),
 	autoprefixer  = require( 'gulp-autoprefixer' ),
-	browserSync   = require( 'browser-sync' ).create(),
+	browserSync   = require( 'browser-sync' ),
+	reload		  = browserSync.reload,
 	babel         = require( 'gulp-babel' ),
 	concat        = require( 'gulp-concat' ),
 	jade          = require( 'gulp-jade' ),
@@ -31,7 +32,7 @@ var settings = {
 
 gulp.task( 'jade', function ()  {
 
-	return gulp.src( `./${settings.srcDir}/jade/*.jade` )
+	return gulp.src( './'+settings.srcDir+'/jade/*.jade' )
 				.pipe( plumber() )
 				.pipe( jade({ pretty: '\t' }) )
 				.on( 'error', function (err) {
@@ -43,13 +44,15 @@ gulp.task( 'jade', function ()  {
 					icon    : '',
 					sound   : 'Basso'
 			   	}))
-				.pipe( gulp.dest( `./${settings.destDir}` ) );
+				.pipe( gulp.dest( './'+settings.destDir+'' ) );
 
 } );
 
+gulp.task( 'jade-watch', ['jade'], reload );
+
 gulp.task( 'style', function ()  {
 
-	return gulp.src( `./${settings.srcDir}/sass/main.scss` )
+	return gulp.src( './'+settings.srcDir+'/sass/main.scss' )
 				.pipe( plumber() )
 				.pipe( sourcemap.init() )
 				.pipe( sass().on('error',sass.logError) )
@@ -62,49 +65,49 @@ gulp.task( 'style', function ()  {
 				.on('error', function () { this.emit( 'end' ) })
 				.pipe( autoprefixer({browsers: "last 3 version"}) )
 				.pipe( sourcemap.write( '.' ) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/css/` ) )
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/css/' ) )
 				.pipe( browserSync.stream({match: '**/*.css'}) )
 				.pipe( uglifyCSS() )
 				.pipe( rename({suffix: '.min'}) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/css/` ) );
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/css/' ) );
 
 } );
 
 
 gulp.task( 'script', function ()  {
 
-	return gulp.src( `./${settings.srcDir}/js/main.js` )
+	return gulp.src( './'+settings.srcDir+'/js/main.js' )
 				.pipe( plumber() )
 				.pipe( jshint()  )
 				.pipe( jshint.reporter( stylish ) )
 				.pipe( babel({ "presets": ["es2015"] }) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/js/` ) )
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/js/' ) )
 				.pipe( sourcemap.init() )
 				.pipe( uglifyJs() )
 				.pipe( rename({ suffix: '.min' }) )
 				.pipe( sourcemap.write( '.' ) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/js` ) );
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/js' ) );
 
 });
 
 gulp.task( 'script-plugins', function ()  {
 
-	return gulp.src( `./${settings.srcDir}/js/plugins/**/*.js` )
+	return gulp.src( './'+settings.srcDir+'/js/plugins/**/*.js' )
 				.pipe( concat( 'plugins.js' ) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/js` ) )
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/js' ) )
 				.pipe( sourcemap.init() )
 				.pipe( uglifyJs() )
 				.pipe( rename({ suffix: '.min' }) )
 				.pipe( sourcemap.write( '.' ) )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/js` ) );
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/js' ) );
 
 } );
 
 gulp.task( 'image', function () {
 
-	return gulp.src( `./${settings.destDir}/assets/images/*` )
+	return gulp.src( './'+settings.destDir+'/assets/images/*' )
 				.pipe( optimizeImage() )
-				.pipe( gulp.dest( `./${settings.destDir}/assets/images` ) );
+				.pipe( gulp.dest( './'+settings.destDir+'/assets/images' ) );
 
 } );
 
@@ -116,12 +119,12 @@ gulp.task( 'cleanup', function () {
 
 gulp.task( 'build-zip', function () {
 
-	return gulp.src( `./${settings.destDir}/**/` )
-				.pipe( zip( settings.projectName+`-${settings.version}`+'.zip' ) )
+	return gulp.src( './'+settings.destDir+'/**/' )
+				.pipe( zip( settings.projectName+'-${settings.version}'+'.zip' ) )
 				.pipe( gulp.dest('./') )
 				.pipe( notify({
 					title   : settings.projectName,
-					message : "Bundling Project Completed. Output file: "+settings.projectName+`-${settings.version}`+".zip",
+					message : "Bundling Project Completed. Output file: "+settings.projectName+'-${settings.version}'+".zip",
 					sound   : false,
 					icon    : false,
 					onLast  : true
@@ -151,29 +154,23 @@ gulp.task( 'watch', function ()  {
 	console.log( "\x1b[31m%s\x1b[32m%s\x1b[35m%s\x1b[0m\x1b[31m%s","║","   Source :"," http://ariona.github.com/frontend-kickstarter   ","║" );
 	console.log( "\x1b[31m%s","╚════════════════════════════════════════════════════════════╝" );	
 
-	browserSync.init({
-		server: {
-			baseDir:`./${settings.destDir}/`
-		}
-	});
+	browserSync({ server: './'+settings.destDir+'/' });
 
-	watch( `./${settings.srcDir}/jade/**/*.jade`, function () {
-		gulp.start( 'jade', function () {			
-			browserSync.reload();
-		} );
+	watch( './'+settings.srcDir+'/jade/**/*.jade', function () {
+		gulp.start( 'jade-watch' );
 	} );
 
-	watch( `./${settings.srcDir}/sass/**/*.scss`, function () {
+	watch( './'+settings.srcDir+'/sass/**/*.scss', function () {
 		gulp.start( 'style' )	;
 	} );
 
-	watch( `./${settings.srcDir}/js/*.js`, function ()  {
+	watch( './'+settings.srcDir+'/js/*.js', function ()  {
 		gulp.start( 'script', function ()  {
 			browserSync.reload();
 		} );
 	} );
 
-	watch( `./${settings.srcDir}/js/plugins/*.js`, function ()  {
+	watch( './'+settings.srcDir+'/js/plugins/*.js', function ()  {
 		gulp.start( 'script-plugins', function ()  {
 			browserSync.reload();
 		} );
